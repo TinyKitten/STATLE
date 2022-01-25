dffe74570e515f7989ba5d8c1856c7817a9fcdee
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2022-01-26 00:54:42 +0900

    OGP画像更新

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -52,7 +52,7 @@ export default class MyDocument extends Document {
           <meta property="og:type" content="website" />
           <meta
             property="og:image"
-            content="https://statle.tinykitten.me/ogp-202201250253.png"
+            content="https://statle.tinykitten.me/ogp-202201260054.png"
           />
           <meta property="og:image:width" content="1200" />
           <meta property="og:image:height" content="630" />
