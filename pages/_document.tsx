481953b65f859d531a0a99cd457074e39f7be23b
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2022-01-26 11:17:44 +0900

    favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -57,6 +57,27 @@ export default class MyDocument extends Document {
           <meta property="og:image:width" content="1200" />
           <meta property="og:image:height" content="630" />
           <meta name="twitter:card" content="summary_large_image" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0071be" />
+          <meta name="msapplication-TileColor" content="#0071be" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
