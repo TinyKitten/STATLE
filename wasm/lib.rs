89e560eefa9b54e4e519be9ff9e9bfab16061806
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2022-01-30 00:25:30 +0900

    シード生成修正アルゴリズム修正

---
+++ wasm/lib.rs
@@ -0,0 +1,17 @@
+use chrono::{Datelike, Local};
+use std::env;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn generate() -> u32 {
+    let local_date = Local::now();
+    let salt_env = env!("SALT");
+    let mul_env = env!("MUL");
+    let salt = salt_env.parse::<u32>().unwrap();
+    let mul = mul_env.parse::<u32>().unwrap();
+    let yr = local_date.year() as u32;
+    let mo = local_date.month() as u32;
+    let dy = local_date.day() as u32;
+
+    (yr + mo + dy + salt) % mul
+}
