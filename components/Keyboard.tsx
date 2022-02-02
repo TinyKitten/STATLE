f50e97ed914a1ada9ebe30b2fe662c11a2811ce2
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2022-02-03 04:19:32 +0900

    見切れ対策

--- components/Keyboard.tsx
+++ components/Keyboard.tsx
@@ -6,9 +6,9 @@ type Props = {
 };
 
 const KEYS: string[][] = [
-  ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "⌫"],
-  ["A", "S", "D", "F", "G", "H", "J", "K", "L", "ENTER"],
-  ["Z", "X", "C", "V", "B", "N", "M"],
+  ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+  ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+  ["ENTER", "Z", "X", "C", "V", "B", "N", "M", "⌫"],
 ];
 
 const Container = styled.div`
