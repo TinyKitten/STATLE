d67f9cbd8b62ba82592e78df9bbffb6e45786d31
makotia <3mdevmail@gmail.com>
2022-02-12 22:42:16 +0900

    Revert "フォーマットなおせるようにした"
    This reverts commit 855c85494b2710a881e025085263dc2822c16a7e.

--- wasm/seedgen_bg.js
+++ wasm/seedgen_bg.js
@@ -1,89 +1,79 @@
-import * as wasm from "./seedgen_bg.wasm";
+import * as wasm from './seedgen_bg.wasm';
 
 const heap = new Array(32).fill(undefined);
 
 heap.push(undefined, null, true, false);
 
-function getObject(idx) {
-  return heap[idx];
-}
+function getObject(idx) { return heap[idx]; }
 
 let heap_next = heap.length;
 
 function dropObject(idx) {
-  if (idx < 36) return;
-  heap[idx] = heap_next;
-  heap_next = idx;
+    if (idx < 36) return;
+    heap[idx] = heap_next;
+    heap_next = idx;
 }
 
 function takeObject(idx) {
-  const ret = getObject(idx);
-  dropObject(idx);
-  return ret;
+    const ret = getObject(idx);
+    dropObject(idx);
+    return ret;
 }
 
-const lTextDecoder =
-  typeof TextDecoder === "undefined"
-    ? (0, module.require)("util").TextDecoder
-    : TextDecoder;
+const lTextDecoder = typeof TextDecoder === 'undefined' ? (0, module.require)('util').TextDecoder : TextDecoder;
 
-let cachedTextDecoder = new lTextDecoder("utf-8", {
-  ignoreBOM: true,
-  fatal: true,
-});
+let cachedTextDecoder = new lTextDecoder('utf-8', { ignoreBOM: true, fatal: true });
 
 cachedTextDecoder.decode();
 
 let cachegetUint8Memory0 = null;
 function getUint8Memory0() {
-  if (
-    cachegetUint8Memory0 === null ||
-    cachegetUint8Memory0.buffer !== wasm.memory.buffer
-  ) {
-    cachegetUint8Memory0 = new Uint8Array(wasm.memory.buffer);
-  }
-  return cachegetUint8Memory0;
+    if (cachegetUint8Memory0 === null || cachegetUint8Memory0.buffer !== wasm.memory.buffer) {
+        cachegetUint8Memory0 = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachegetUint8Memory0;
 }
 
 function getStringFromWasm0(ptr, len) {
-  return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
+    return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
 }
 /**
- * @returns {number}
- */
+* @returns {number}
+*/
 export function generate() {
-  var ret = wasm.generate();
-  return ret >>> 0;
+    var ret = wasm.generate();
+    return ret >>> 0;
 }
 
 function addHeapObject(obj) {
-  if (heap_next === heap.length) heap.push(heap.length + 1);
-  const idx = heap_next;
-  heap_next = heap[idx];
+    if (heap_next === heap.length) heap.push(heap.length + 1);
+    const idx = heap_next;
+    heap_next = heap[idx];
 
-  heap[idx] = obj;
-  return idx;
+    heap[idx] = obj;
+    return idx;
 }
 
 export function __wbindgen_object_drop_ref(arg0) {
-  takeObject(arg0);
-}
+    takeObject(arg0);
+};
 
 export function __wbg_getTime_10d33f4f2959e5dd(arg0) {
-  var ret = getObject(arg0).getTime();
-  return ret;
-}
+    var ret = getObject(arg0).getTime();
+    return ret;
+};
 
 export function __wbg_getTimezoneOffset_d3e5a22a1b7fb1d8(arg0) {
-  var ret = getObject(arg0).getTimezoneOffset();
-  return ret;
-}
+    var ret = getObject(arg0).getTimezoneOffset();
+    return ret;
+};
 
 export function __wbg_new0_fd3a3a290b25cdac() {
-  var ret = new Date();
-  return addHeapObject(ret);
-}
+    var ret = new Date();
+    return addHeapObject(ret);
+};
 
 export function __wbindgen_throw(arg0, arg1) {
-  throw new Error(getStringFromWasm0(arg0, arg1));
-}
+    throw new Error(getStringFromWasm0(arg0, arg1));
+};
+
